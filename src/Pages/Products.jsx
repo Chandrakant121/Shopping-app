9ed3bd5ab00d6cd58784e9eb894276c07a8226db
Chandrakant121 <csjoshi989@gmail.com>
2022-07-12 14:03:05 +0530

    updated navbar

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Box } from "@chakra-ui/react"
+import FilterComponent from '../Components/FilterComponent'
+
 
 const Products = () => {
-    return (
-        <div>Products</div>
-    )
+    return <>
+        <Box>
+            <Box>
+                <FilterComponent />
+            </Box>
+
+            <Box>
+                {/* Products */}
+            </Box>
+
+        </Box>
+    </>
 }
 
 export default Products
