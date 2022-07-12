fb6aafb9ec7740b90094d9f25ad52df0618ce589
Chandrakant121 <csjoshi989@gmail.com>
2022-07-12 13:35:20 +0530

    addede components navbar

---
+++ src/Components/AllRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Cart from '../Pages/Cart'
+import Homepage from '../Pages/Homepage'
+import Products from '../Pages/Products'
+import Product from '../Pages/product'
+
+const AllRoutes = () => {
+    return <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+    </Routes>
+}
+
+export default AllRoutes
